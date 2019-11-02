b32fde327b2ff616a3354d0091e5e426ed56a2fd
ikaddoura <kaddoura@vsp.tu-berlin.de>
2019-11-02 08:54:23 +0100

    minor

--- src/main/java/org/matsim/berlin/RunOpenBerlinWithDRTSpeedUp.java
+++ src/main/java/org/matsim/berlin/RunOpenBerlinWithDRTSpeedUp.java
@@ -41,7 +41,7 @@ public class RunOpenBerlinWithDRTSpeedUp {
 		}
 		
 		if ( args.length==0 ) {
-			args = new String[] {"scenarios/berlin-v5.5-1pct/input/drt/berlin-drt-v5.5-1pct.config.xml"}  ;
+			args = new String[] {"test/input/berlin-drt-v5.5-1pct.config.xml"}  ;
 		}
 		
 		Config config = RunDrtOpenBerlinScenario.prepareConfig( args ) ;
