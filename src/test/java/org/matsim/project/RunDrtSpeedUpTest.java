e67e3ef08e2c3755d1e80c56797b2bc3141c4f73
ikaddoura <kaddoura@vsp.tu-berlin.de>
2019-11-07 15:42:19 +0100

    get rid of berlin test classes

---
+++ src/test/java/org/matsim/project/RunDrtSpeedUpTest.java
@@ -0,0 +1,23 @@
+package org.matsim.project;
+
+import org.junit.FixMethodOrder;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+import org.matsim.testcases.MatsimTestUtils;
+
+/**
+ * 
+ * @author ikaddoura
+ *
+ */
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class RunDrtSpeedUpTest {
+		
+	@Rule public MatsimTestUtils utils = new MatsimTestUtils() ;
+	
+	@Test
+	public final void test1() {
+		
+	}
+}
